c6bd26f8226773ad7bc1a58a8060e5874a736141
Soumya Ranjan Das <soumyazyx@gmail.com>
2020-05-03 10:11:55 +0530

    heroku specific changes

--- api/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Task(models.Model):
-    title = models.CharField(max_length=200)
-    completed = models.BooleanField(defualt=False, blank=True, null=True)
-
-    def __str__(self):
-        return self.titleF
