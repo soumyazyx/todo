fa520537ef0d66cae4d82db14092270ec3bab995
Soumya Ranjan Das <soumyazyx@gmail.com>
2020-05-05 01:17:34 +0530

    Added LIST related end points

--- api/urls.py
+++ api/urls.py
@@ -3,10 +3,19 @@ from . import views
 
 
 urlpatterns = [
+
     path('', views.apiOverview, name="api-overview"),
-    path('task-list/', views.taskList, name="task-list"),
+
+    path('tasks/',               views.tasks,      name="tasks"),
     path('task-detail/<str:pk>', views.taskDetail, name="task-detail"),
-    path('task-create/', views.taskCreate, name="task-create"),
+    path('task-create/',         views.taskCreate, name="task-create"),
     path('task-update/<str:pk>', views.taskUpdate, name="task-update"),
     path('task-delete/<str:pk>', views.taskDelete, name="task-delete"),
+
+    path('lists/',               views.lists,      name="lists"),
+    path('list-detail/<str:pk>', views.listDetail, name="list-detail"),
+    path('list-create/',         views.listCreate, name="list-create"),
+    path('list-update/<str:pk>', views.listUpdate, name="list-update"),
+    path('list-delete/<str:pk>', views.listDelete, name="list-delete"),
+
 ]
--- api/views.py
+++ api/views.py
@@ -3,25 +3,30 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .models import Task
-from .serializers import TaskSerializer
+from .models import Task, List
+from .serializers import TaskSerializer, ListSerializer
 
 
 @api_view(['GET'])
 def apiOverview(request):
     api_urls = {
-        'List': '/task-list/',
-        'Detail View': '/task-detail/<str:pk>/',
-        'Create': '/task-create/',
-        'Update': '/task-update/<str:pk>/',
-        'Delete': '/task-delete/<str:pk>/',
-    }
+        'Tasks': '/tasks/',
+        'TaskDetail': '/task-detail/<str:pk>/',
+        'TaskCreate': '/task-create/',
+        'TaskUpdate': '/task-update/<str:pk>/',
+        'TaskDelete': '/task-delete/<str:pk>/',
 
+        'Lists': '/lists/',
+        'ListDetail': '/list-detail/<str:pk>/',
+        'ListCreate': '/list-create/',
+        'ListUpdate': '/list-update/<str:pk>/',
+        'ListDelete': '/list-delete/<str:pk>/',
+    }
     return Response(api_urls)
 
 
 @api_view(['GET'])
-def taskList(request):
+def tasks(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
@@ -56,3 +61,41 @@ def taskDelete(request, pk):
     task = Task.objects.get(id=pk)
     task.delete()
     return Response("Item succesfully deleted!")
+
+
+@api_view(['GET'])
+def lists(request):
+    lists = List.objects.all()
+    serializer = ListSerializer(lists, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def listDetail(request, pk):
+    _list = List.objects.get(id=pk)
+    serializer = ListSerializer(_list, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def listCreate(request):
+    serializer = ListSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def listUpdate(request, pk):
+    _list = List.objects.get(id=pk)
+    serializer = TaskSerializer(instance=_list, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def listDelete(request, pk):
+    _list = List.objects.get(id=pk)
+    _list.delete()
+    return Response("List successfully deleted!")
