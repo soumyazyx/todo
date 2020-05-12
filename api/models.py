94e15ee9286fb72950d8ec7dac33299b83bdbe2c
Soumya Ranjan Das <soumyazyx@gmail.com>
2020-05-12 11:28:57 +0530

    Minor formatting changes

--- api/models.py
+++ api/models.py
@@ -3,16 +3,15 @@ from django.conf import settings
 
 
 class List(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
 
     def __str__(self):
-        return '{}|{}'.format(self.user.username, self.title)
+        return "{}".format(self.title)
 
 
 class Task(models.Model):
-    _list = models.ForeignKey(List, on_delete=models.CASCADE, default=2)
+    _list = models.ForeignKey(List, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False, null=True, blank=True)
 
